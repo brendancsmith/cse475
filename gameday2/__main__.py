ffa3b5919cafed13f04e53d88afffa97b48d1ffb
Brendan Smith <brendan.smith.93@gmail.com>
2015-03-02 00:25:12 -0600

    Implement plurality voting example.

--- gameday2/__main__.py
+++ gameday2/__main__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python27
 
 import gameday2
+
+print gameday2.vote_plurality_example()
--- gameday2/gameday2.py
+++ gameday2/gameday2.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python27
 # encoding: utf-8
+
+from ballotbox.ballot import BallotBox
+from ballotbox.singlewinner.plurality import FirstPastPostVoting
+
+
+def vote_plurality_example():
+    bb = BallotBox(method=FirstPastPostVoting)
+    bb.batch_votes([("alice", 4000), ("bob", 3000), ("carol", 5000)])
+
+    preference = bb.get_winner(position_count=3)
+    return preference
